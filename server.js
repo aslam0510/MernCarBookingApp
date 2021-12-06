f205b48c65689d31168680ba927bfc613f58b8bd
Aslam Khan <aslam51096@gmail.com>
2021-12-07 02:27:52 +0530

    added

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use('/api/bookings',carBooingRoute);
 
 if(process.env.NODE_ENV === 'production'){
   app.use('/',express.static('frontend/build'));
+  
   app.get('*', (req,res)=>{
     res.sendFile(path.resolve(__dirname,'frontend', 'build' ,'index.html'))
   })
