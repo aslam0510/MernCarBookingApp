499e4ea6bcadea8be0b4edfc0f019cc0cad1aaf1
Aslam Khan <aslam51096@gmail.com>
2021-12-07 20:46:43 +0530

    commited

--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  userName : {type:String, required:true},
-  password : {type:String, required:true}
-})
-
-const userModel = mongoose.model('users', userSchema);
-module.exports = userModel;
