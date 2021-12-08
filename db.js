1c1afea6a78ead52a64994cd2fccfb80f3c8f554
Aslam Khan <aslam51096@gmail.com>
2021-12-08 09:23:01 +0530

    connecting to mongodb

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ function connectDB(){
   // 'mongodb+srv://aslam05:aslam05@cluster0.xkgan.mongodb.net/carapp'
   
   const URL = 'mongodb+srv://aslam05:aslam05@cluster0.xkgan.mongodb.net/carapp?retryWrites=true&w=majority'
-  mongoose.connect(process.env.MONGO_URL || URL,{useUnifiedTopology:true, useNewUrlParser:true});
+  mongoose.connect(process.env.MONGO_URI || URL,{useUnifiedTopology:true, useNewUrlParser:true});
 
   const connection = mongoose.connection;
   
